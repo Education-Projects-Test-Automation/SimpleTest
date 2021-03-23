fd3f4f4862dcdc97cd0a7fc425dd89b246e78a87
Mykhailo Kurshakov <Mykhailo_Kurshakov@epam.com>
2021-03-23 22:43:22 +0200

    Add project files.

---
+++ SimpleTest/FirstTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+
+namespace SimpleTest
+{
+    class FirstTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new FirefoxDriver();
+
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://www.wikipedia.org/";
+            IWebElement searchField = driver.FindElement(By.XPath("//input[@id=\"searchInput\"]"));
+
+            searchField.SendKeys("automation testing");
+            IWebElement searchButton = driver.FindElement(By.XPath("//*[@id=\"search-form\"]/fieldset/button"));
+            searchButton.Click();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
